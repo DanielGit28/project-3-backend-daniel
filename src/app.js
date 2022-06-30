d9bdd06c35a73827e4f3b751cb2b46422cadfa61
duniel12 <duniel12@gmail.com>
2022-06-30 15:43:35 -0600

    middleware credentials test

--- src/app.js
+++ src/app.js
@@ -53,6 +53,7 @@ app.post("/*", postMiddleware)
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 app.get("/", (req, res, next) => {
