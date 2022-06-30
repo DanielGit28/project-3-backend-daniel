4b8af20fe88b1f51e4175802e7581e461a9a1d13
duniel12 <duniel12@gmail.com>
2022-06-30 15:50:02 -0600

    cors options disabled

--- src/app.js
+++ src/app.js
@@ -34,13 +34,9 @@ import accountMovementsRouter from "./routes/account-movement.route.js";
 import servicesRouter from "./routes/service.route.js";
 
 import cors from "cors";
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 
 app.use(bp.json());
