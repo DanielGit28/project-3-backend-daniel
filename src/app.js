55d137bc017d52b3b375cf95338913ead6d762a2
duniel12 <duniel12@gmail.com>
2022-06-30 22:28:15 -0600

    cors test

--- src/app.js
+++ src/app.js
@@ -66,7 +66,7 @@ app.use(loggerMiddleware);
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', ['http://localhost:3000','http://localhost:3000/']);
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   
   //res.header('Access-Control-Allow-Credentials', true);
   next();
